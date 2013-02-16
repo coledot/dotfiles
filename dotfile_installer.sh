4dd7ec2f8c08e3df1e70230f26906a1b98b71429
Cole Thompson <cole.thompson@gmail.com>
2013-02-16 15:56:35 -0600

    Added installer script
    Minor fix/workaround to .vimrc as well.

---
+++ dotfile_installer.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+cd $HOME
+filelist=`find .dotfiles -mindepth 1 -maxdepth 1 -not -name .git`
+
+dotfile_bkp_dir=".dotfile_bak"
+
+if [[ ! -d $dotfile_bkp_dir ]]; then
+	mkdir $dotfile_bkp_dir
+fi
+
+for true_dotfile in $filelist; do
+	symlink_dotfile=`echo $true_dotfile | sed 's,\\.dotfiles/,,g'`
+	old_dotfile=$symlink_dotfile
+	if [[ ! -h $symlink_dotfile ]]; then
+		if [[ -e $old_dotfile ]]; then
+			mv $old_dotfile $dotfile_bkp_dir
+		fi
+
+		ln -sf $true_dotfile $symlink_dotfile
+		if [[ $? -ne 0 ]]; then
+			echo "failed to symlink $symlink_dotfile to $true_dotfile"
+		fi
+	fi
+done
+
